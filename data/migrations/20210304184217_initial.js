262ec688867d8a87c561d501e29861c42cef115b
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-03-07 11:44:46 -0700

    finished building tables and started seeds

--- data/migrations/20210304184217_initial.js
+++ data/migrations/20210304184217_initial.js
@@ -1,8 +1,32 @@
 
 exports.up = async function(knex) {
-  
+	await knex.schema.createTable("recipes", (table) => {
+		table.increments("id")
+		table.text("name").notNull().unique()
+	})
+
+	await knex.schema.createTable("ingredients", (table) => {
+		table.increments("id")
+		table.text("name").notNull().unique()
+	})
+
+	await knex.schema.createTable("instructions", (table) => {
+		table.increments("id")
+		table.text('step').notNull().unique()
+		table.integer("recipe_id").notNull().references("id").inTable("recipes").onDelete("CASCADE").onUpdate("CASCADE")
+	})
+
+	await knex.schema.createTable("recipeDetails", (table) => {
+		table.increments("id")
+		table.integer("recipe_id").notNull().references("id").inTable("recipes").onDelete("CASCADE").onUpdate("CASCADE")
+		table.integer("ingredient_id").notNull().references("id").inTable("ingredients").onDelete("CASCADE").onUpdate("CASCADE")
+		table.integer("quantity").notNull()
+	})
 }
 
 exports.down = async function(knex) {
-  
+	await knex.schema.dropTableIfExists("recipeDetails")
+	await knex.schema.dropTableIfExists("instructions")
+	await knex.schema.dropTableIfExists("ingredients")
+	await knex.schema.dropTableIfExists("recipes")
 }
